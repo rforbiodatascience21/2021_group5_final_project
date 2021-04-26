ea6a142e4dec5d8dfea9a26394a6c32937d1f982
signemarcussen <engis-10@hotmail.com>
2021-04-26 08:31:17 +0000

    change dataname

--- R/05_analysis_ii_logistic.R
+++ R/05_analysis_ii_logistic.R
@@ -20,29 +20,29 @@ gordon_clean_aug %>% ...
 
 # Model data ------------------------------------------------------------
 ## Logistic regression model for each of the probes
-gordon_clean_aug <- gordon_clean_aug %>% 
+gordon_logistic <- gordon_clean_aug %>% 
   mutate(mdl = map(data, ~ glm(outcome ~ value, 
                                data = .x,
                                family = binomial(link = "logit"))))
 
 ## Adding statistical variables for intercept and the estimated values for each model
 ## (std.error, statistic,  p.value, conf.low, conf.high)
-gordon_clean_aug <- gordon_clean_aug %>% 
+gordon_logistic <- gordon_logistic %>% 
   mutate(mdl_tidy = map(mdl, tidy, conf.int = TRUE)) %>% 
   unnest(mdl_tidy)
 
 ## As we are not interested in the intercept for the values, we remove these rows
-gordon_clean_aug <- gordon_clean_aug %>% 
+gordon_logistic <- gordon_logistic %>% 
   filter(term == "value")
 
 ## Identify significant and non-significant genes
-gordon_clean_aug <- gordon_clean_aug %>% 
+gordon_logistic <- gordon_logistic %>% 
   mutate(identified_as = case_when(p.value < 0.05 ~ "Significant",
                                    TRUE ~ "Non-significant"))
 
 
 # Visualise data ----------------------------------------------------------
-gordon_clean_aug %>% ...
+gordon_logistic %>% ...
 
 
 # Write data --------------------------------------------------------------
--- R/06_analysis_iii_PCA.R
+++ R/06_analysis_iii_PCA.R
@@ -10,38 +10,19 @@ library("broom")
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
+
 # Load data ---------------------------------------------------------------
 gordon_clean_aug <- readRDS(file = "data/03_gordon_100.rds")
 
-# Model data  -------------------------------------------------------------
-## Logistic regression model for each of the probes
-gordon_clean_aug <- gordon_clean_aug %>% 
-  mutate(mdl = map(data, ~ glm(outcome ~ value, 
-                               data = .x,
-                               family = binomial(link = "logit"))))
 
-## Adding statistical variables for intercept and the estimated values for each model
-## (std.error, statistic,  p.value, conf.low, conf.high)
-gordon_clean_aug <- gordon_clean_aug %>% 
-  mutate(mdl_tidy = map(mdl, tidy, conf.int = TRUE)) %>% 
-  unnest(mdl_tidy)
-
-## As we are not interested in the intercept for the values, we remove these rows
-gordon_clean_aug <- gordon_clean_aug %>% 
-  filter(term == "value")
-
-## Identifying weather the genes are significant or not, based on the P-value
-gordon_clean_aug <- gordon_clean_aug %>% 
-  mutate(identified_as = case_when(p.value < 0.05 ~ "Significant",
-                                   TRUE ~ "Non-significant"))
-
-# PCA analysis ---------------------------------------------------------------
-## Selecting the same 100 genes as previous 
+# Wrangle data  -------------------------------------------------------------
 gordon_data_wide <- gordon_clean_aug %>% select(outcome, pull(gordon_100, probe))
 
 gordon_data_wide <- gordon_data_wide %>% 
   mutate(outcome = as_factor(outcome)) #why is this necessary? 
 
+
+# PCA analysis ---------------------------------------------------------------
 pca_fit <- gordon_data_wide %>% 
   select(where(is.numeric)) %>% 
   prcomp(scale = TRUE)
