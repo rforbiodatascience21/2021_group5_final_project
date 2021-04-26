1e89f4cc01b98f2a612553ca80d865b0b88a61e8
sosbnb <s3359baryern@hotmial.com>
2021-04-26 19:40:21 +0200

    add visualization

--- R/05_analysis_ii_logistic.R
+++ R/05_analysis_ii_logistic.R
@@ -41,9 +41,51 @@ gordon_logistic <- gordon_logistic %>%
 
 
 # Visualise data ----------------------------------------------------------
-gordon_logistic %>% ...
+## Make a Manhattan-plot for illustrating p-values for probe association
+gordon_logistic <- gordon_logistic%>%
+  mutate(neg_log10_p = -log10(p.value))
+
+plot1 <- gordon_logistic %>% 
+  ggplot(mapping = aes(x = fct_reorder(probe, neg_log10_p, .desc = TRUE), 
+                       y = neg_log10_p,
+                       color = identified_as)) +
+  geom_point() + 
+  geom_hline(yintercept = - log10(0.05), linetype = "dashed") +
+  theme_classic(base_size = 8, base_family = "Avenir") +
+  theme(legend.position = "bottom", 
+        axis.text.x = element_text(angle = 45 , vjust = 1, hjust = 1)) +
+  labs( x = "Probe", y = "Minus log10(p)")
+
+## A confidence interval plot with effct directions
+plot2 <- gordon_logistic %>% 
+  ggplot(mapping = aes(x = estimate, y = fct_reorder(probe, estimate, .desc = TRUE),
+                       color = identified_as)) + 
+  geom_point() + 
+  geom_vline(xintercept = 0, linetype = "dashed" ) + 
+  geom_errorbarh(aes(xmin = conf.low,
+                     xmax = conf.high,
+                     height = 0.1)) +
+  theme_classic(base_size = 8, base_family = "Avenir") +
+  theme(legend.position = "bottom" ) +
+  labs( x = "Estimate", y = "Probe")
+
+## A confidence interval plot only including genes with a significant association
+plot3 <- gordon_logistic %>% 
+  filter(identified_as == "Significant") %>% 
+  ggplot(mapping = aes(x = estimate, y = fct_reorder(probe, estimate, .desc = TRUE))) + 
+  geom_point() + 
+  geom_vline(xintercept = 0, linetype = "dashed" ) + 
+  geom_errorbarh(aes(xmin = conf.low,
+                     xmax = conf.high,
+                     height = 0.1)) +
+  theme_classic(base_size = 8, base_family = "Avenir") +
+  theme(legend.position = "bottom" ) +
+  labs( x = "Estimate", y = "Probe")
 
 
 # Write data --------------------------------------------------------------
-write_tsv(...)
-ggsave(...)
+saveRDS(object  = gordon_logistic,
+        file = "results/05_gordon_logistic.rds")
+ggsave(filename = "results/05_logistic_manhattan.png", plot = plot1)
+ggsave(filename = "results/05_logistic_interval.png", plot = plot2)
+ggsave(filename = "results/05_logistic_significant.png", plot = plot3)
